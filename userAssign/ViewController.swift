a2cb3d4c64884787132e49c0482b52bf0a64f449
leslie <leslie@ccs-indy.com>
2020-03-27 20:02:57 -0400

    remove signing

--- userAssign/ViewController.swift
+++ userAssign/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController, URLSessionDelegate {
     var username      = ""
     var password      = ""
     var deviceUdid    = ""
-    var eaShow        = false
+    var eaShow        = "no"
     var eaID          = "0"
     var eaName        = ""
     var popup_choices = [String]()
@@ -122,13 +122,13 @@ class ViewController: UIViewController, URLSessionDelegate {
                 print("status code: \(httpResponse.statusCode)")
 //                print("\(httpResponse.description)")
                 if httpResponse.statusCode >= 199 && httpResponse.statusCode <= 299 {
-                    self.message_TextView.text = "successfully assigned"
+                    self.message_TextView.text      = "successfully assigned"
                     self.message_TextView.textColor = UIColor.green
-                    self.message_TextView.font =  UIFont(name: "HelveticaNeue", size: CGFloat(32))
+                    self.message_TextView.font      =  UIFont(name: "HelveticaNeue", size: CGFloat(32))
                 } else {
-                    self.message_TextView.text = "failed to assigned"
+                    self.message_TextView.text      = "failed to assigned"
                     self.message_TextView.textColor = UIColor.red
-                    self.message_TextView.font =  UIFont(name: "HelveticaNeue", size: CGFloat(32))
+                    self.message_TextView.font      =  UIFont(name: "HelveticaNeue", size: CGFloat(32))
                 }
             }   // if let httpResponse - end
             self.spinner.stopAnimating()
@@ -136,16 +136,10 @@ class ViewController: UIViewController, URLSessionDelegate {
         task.resume()
         // attempt to update - end
     }
-    
-//    override func viewWillAppear(_ animated: Bool) {
-//        print("[viewWillAppear]")
-//        selectedConfig_TextField.text = "\(Preferences.selectedConfig)"
-//    }
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
 
         // Do any additional setup after loading the view.
         // HexColor - Mac App Store
@@ -165,74 +159,71 @@ class ViewController: UIViewController, URLSessionDelegate {
             deviceUdid = configDict["udid"] as! String
             // app config is valid
             if let _ = configDict["eaShow"], let _ = configDict["eaID"] {
-                eaShow = configDict["eaShow"] as! Bool
+                eaShow = configDict["eaShow"] as! String
                 eaID   = configDict["eaID"] as! String
-                if eaShow {
-                    config_Button.isHidden = false
-                    selectedConfig_TextField.isHidden = false
-                }
             }
         } else {
             // handle missing app config here
-            self.message_TextView.text = "Missing App Config"
+            self.message_TextView.text      = "Missing App Config"
             self.message_TextView.textColor = UIColor.red
-            self.message_TextView.font =  UIFont(name: "HelveticaNeue", size: CGFloat(32))
+            self.message_TextView.font      =  UIFont(name: "HelveticaNeue", size: CGFloat(32))
             // for simulator testing without app config
-            baseUrl    = "https://m.hickoryhillseast.net"
-            username   = "testUserAssign"
+            baseUrl    = "https://your.jamf.server"
+            username   = "userAssign"
             password   = "S3cr3t"
-            deviceUdid = "80023a617b92bbc7aca27463a3df5ac0b188a654"   // test device udid
-            eaShow     = true
-            eaID       = "18"
-            if eaShow {
-                config_Button.isHidden = false
-                selectedConfig_TextField.isHidden = false
-            }
+            deviceUdid = "80023a617b92bbcb3b3b3b33a3df5ac0b188a654"   // test device udid
+            eaShow     = "yes"
+            eaID       = "19"
         }   // if let configDict - end
         
-        // pull list for dropdown from EA
-                let attributeURL = "\(baseUrl)/JSSResource/mobiledeviceextensionattributes/id/\(eaID)"
-                let creds        = "\(username):\(password)"
-                let base64Creds  = creds.data(using: .utf8)?.base64EncodedString()
-                // attempt to update - start
-                URLCache.shared.removeAllCachedResponses()
-                
-                let encodedUrl = NSURL(string: attributeURL)
-                let request = NSMutableURLRequest(url: encodedUrl! as URL)
-                request.httpMethod = "GET"
+        // pull list for dropdown from EA - make sure account can read Mobile Device Extension Attributes
+        let attributeURL = "\(baseUrl)/JSSResource/mobiledeviceextensionattributes/id/\(eaID)"
+        let creds        = "\(username):\(password)"
+        let base64Creds  = creds.data(using: .utf8)?.base64EncodedString()
+        // attempt to update - start
+        URLCache.shared.removeAllCachedResponses()
+        
+        let encodedUrl = NSURL(string: attributeURL)
+        let request = NSMutableURLRequest(url: encodedUrl! as URL)
+        request.httpMethod = "GET"
 //                request.httpBody = assignXml.data(using: String.Encoding.utf8)
-                let configuration = URLSessionConfiguration.default
-                configuration.httpAdditionalHeaders = ["Authorization" : "Basic \(String(describing: base64Creds!))", "Accept" : "application/json"]
-                
-                let session = Foundation.URLSession(configuration: configuration, delegate: self as URLSessionDelegate, delegateQueue: OperationQueue.main)
-                let task = session.dataTask(with: request as URLRequest, completionHandler: {
-                    (data, response, error) -> Void in
-                    if let httpResponse = response as? HTTPURLResponse {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = ["Authorization" : "Basic \(String(describing: base64Creds!))", "Accept" : "application/json"]
+        
+        let session = Foundation.URLSession(configuration: configuration, delegate: self as URLSessionDelegate, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (data, response, error) -> Void in
+            if let httpResponse = response as? HTTPURLResponse {
 //                        print("httpResponse: \(httpResponse)")
-                        do {
-                            let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments)
-                            if let dataJSON = json as? [String:Any] {
-                                print("dataJSON: \(dataJSON)")
-                                let eaAttributes = dataJSON["mobile_device_extension_attribute"] as! [String:Any]
-                                self.eaName = eaAttributes["name"] as! String
-                                
-                                self.config_Button.setTitle("\(self.eaName): ", for: .normal)
-                                
-                                guard let _ = eaAttributes["input_type"] else { return }
-                                let input_type = eaAttributes["input_type"] as! [String:Any]
-                                if input_type["type"] as! String == "Pop-up Menu" {
-                                    self.popup_choices = input_type["popup_choices"] as! [String]
-                                    print("popup_choices for \(self.eaName): \(self.popup_choices)")
-                                }
+                do {
+                    let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+                    if let dataJSON = json as? [String:Any] {
+//                        print("dataJSON: \(dataJSON)")
+                        let eaAttributes = dataJSON["mobile_device_extension_attribute"] as! [String:Any]
+                        self.eaName = eaAttributes["name"] as! String
+                        
+                        self.config_Button.setTitle("\(self.eaName): ", for: .normal)
+                        
+                        guard let _ = eaAttributes["input_type"] else { return }
+                        let input_type = eaAttributes["input_type"] as! [String:Any]
+                        if input_type["type"] as! String == "Pop-up Menu" {
+                            self.popup_choices = input_type["popup_choices"] as! [String]
+//                            print("popup_choices for \(self.eaName): \(self.popup_choices)")
+                        }
+                        
+                        DispatchQueue.main.async {
+                            if self.eaShow.lowercased() == "yes" {
+                                self.config_Button.isHidden = false
+                                self.selectedConfig_TextField.isHidden = false
                             }
                         }
-                    }   // if let httpResponse - end
-                    self.spinner.stopAnimating()
-                })
-                task.resume()
-                // attempt to update - end
-        
-        
+                    }
+                }
+            }   // if let httpResponse - end
+            self.spinner.stopAnimating()
+        })
+        task.resume()
+        // attempt to update - end
     }
     
     func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping(  URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
